30678c701195595d0252c684cc32169841f984af
Waliddeu <walid.daouadi@gmail.com>
2023-12-19 16:39:09 +0100

    initial commit

---
+++ read.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+# Reading Images
+
+img = cv.imread('cat-large.png')
+cv.imshow('Cat', img)
+
+# Reading Videos
+
+capture = cv.VideoCapture('dog-video.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+    
+capture.release()
+cv.destryAllWindows()
+cv.waitKey(0)
+
+
